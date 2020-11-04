9f922b462cf3d9fad94f92005e2fe8434ba88a08
Cesar Lubini <cesar.lubini@gmail.com>
2020-11-04 18:11:59 -0300

    adicionado componente com state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-function App() {
-  return (
-    <h1>hello world!!</h1>
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+
+    this.setState(novoEstado);
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro
+          criarNota={this.criarNota.bind(this)}
+        />
+        <ListaDeNotas
+          notas={this.state.notas}
+        />
+      </section>
+    );
+  }
 }
 
 export default App;
